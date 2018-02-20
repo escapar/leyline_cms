a13720c2c34d173c6ed39fe51bff9ee57fb9dcd3
ThrowableFactory <ThrowableFactory@foxmail.com>
2018-02-20 20:21:46 +0800

    vaadin

---
+++ src/main/java/com/k41d/cms/interfaces/view/admin/AdminIndexPage.java
@@ -0,0 +1,11 @@
+package com.k41d.cms.interfaces.view.admin;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.Route;
+
+@Route("/admin/index")
+public class AdminIndexPage extends Div {
+    public AdminIndexPage(){
+        setText("Hello world!");
+    }
+}
